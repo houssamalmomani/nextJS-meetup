466ab9d5bd0352884971dbd46a1818937b33ff43
houssam almomani <houssamalmomani1@gmail.com>
2022-12-01 11:09:46 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+	{
+		id: 'm1',
+		title: 'A First Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Some address 5,123 Some City',
+		description: 'This is a first meetup!',
+	},
+	{
+		id: 'm2',
+		title: 'A Second Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Some address 10,123 Some City',
+		description: 'This is a Second meetup!',
+	},
+	{
+		id: 'm3',
+		title: 'A third Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Some address 15,123 Some City',
+		description: 'This is a third meetup!',
+	},
+];
+function HomePage() {
+	return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
